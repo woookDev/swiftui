c6c8bca696d10433ce03218b74002faaec3a4c2f
woookdev <hjw0911@gmail.com>
2021-03-14 14:41:38 +0900

    Add ScoreView in Kuchi

--- Kuchi/Sources/App/Practice/ScoreView.swift
+++ Kuchi/Sources/App/Practice/ScoreView.swift
@@ -1,15 +1,15 @@
-/// Copyright (c) 2019 Razeware LLC
-///
+/// Copyright (c) 2021 Razeware LLC
+/// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-///
+/// 
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-///
+/// 
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,6 +17,10 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
+/// 
+/// This project and source code may use libraries or frameworks that are
+/// released under various Open-Source licenses. Use of those libraries and
+/// frameworks are governed by their own individual licenses.
 ///
 /// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 /// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -26,40 +30,22 @@
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
 
-import Combine
 import SwiftUI
 
-struct ScoreView {
-  
-  @Binding private var answered: Int
-  
-  private let questions: Int
-  
-  init(answered: Binding<Int>, of questions: Int) {
-    self._answered = answered
-    self.questions = questions
-  }
-}
-
-extension ScoreView: View {
+struct ScoreView: View {
+  var numberOfAnswered = 0
+  var numberOfQuestions = 5
   
   var body: some View {
     HStack {
-      Text("\(answered)/\(questions)")
-        .font(.caption)
-        .padding(4)
+      Text("\(numberOfAnswered)/\(numberOfQuestions)")
       Spacer()
     }
   }
-  
 }
 
-#if DEBUG
 struct ScoreView_Previews: PreviewProvider {
-  @State static private var score = 42
-  
   static var previews: some View {
-    return ScoreView(answered: $score, of: 500)
+    ScoreView()
   }
 }
-#endif
